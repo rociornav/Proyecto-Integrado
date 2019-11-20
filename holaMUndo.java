c13a4170e265448bc2169970e2da157c462a17a8
rdelr <alum.rdelr@iesalixar.org>
2019-11-20 15:12:25 +0100

    Colores definidos

--- holaMUndo.java
+++ holaMUndo.java
@@ -1,4 +1,17 @@
-Primer fichero 
 
-Hola MUndo.
-Modificación  20/11/19. 
+
+
+public class HolaMundo {
+	public static void main(String[] args){
+		String naranja="\033[33m";
+		String azul="\033[34m";
+		System.out.println(naranja+"¡Hola mundo!");
+		System.out.println(azul+"Aquí estamos haciendo pruebas con Git.");
+	}
+
+
+}
+//Primer fichero 
+
+//Hola MUndo.
+//Modificación  20/11/19. 
